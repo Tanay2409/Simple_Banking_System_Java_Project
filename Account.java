6ab22c1cbd65daca1dc978938c29d54ca36d95af
Tanay2409 <86257435+Tanay2409@users.noreply.github.com>
2021-09-16 19:14:44 +0530

    initial commit

---
+++ Account.java
@@ -0,0 +1,40 @@
+//Base class
+//Abstract
+
+public abstract class Account{
+
+    //account number
+    private int accountNumber;
+
+    //Balance
+    protected double balance;
+
+    //Default constructor
+    public Account(){
+
+    }
+
+    public Account(int accountNumber){
+
+        this.accountNumber = accountNumber;
+        balance = 0.0;
+    }
+
+    //Getter methods
+    public double getBalance(){
+        
+        return this.balance;
+    }
+
+    public int getAccountNumber(){
+
+        return this.accountNumber;
+    }
+
+    //Abstract methods
+
+    public abstract void deposit(double amount);
+
+    public abstract void withdraw(double amount);
+
+}
